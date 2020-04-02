690a4122ff95e302c89183e9c13b575463d74eac
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-04-02 20:13:11 +0200

    Refactor ratios

--- rebalancing.hs
+++ rebalancing.hs
@@ -1,6 +1,8 @@
 -- Calculate ratio of each element in regards to the sum of the elements
-ratios :: (Real a) => [a] -> [Rational]
+--ratios :: (Real a) => [a] -> [Rational]
 --ratios x = [ (toRational el) / toRational (sum x) | el <- x ]
-ratios list = [ toRational el / toRational s  | el <- list, let s = sum list ]
+--ratios list = [ toRational el / toRational s  | el <- list, let s = sum list ]
+ratios list = let size = toRational . sum $ list
+               in map (/ size) list
 
 --deviation x = [ portion % target - 1 | portion <- fst x, target <- snd x ]

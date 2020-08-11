e1a0faa192baa712b13d1b5c702be0ee36dae9af
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-08-11 23:47:01 +0200

    let's goooo!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,33 @@
-module Main where
+{-# LANGUAGE ParallelListComp #-}
 
-import Lib
+module Lib where
 
-main :: IO ()
-main = someFunc
+-- calculate :: Target, [Assets] -> [Contribution]
+-- fractions :: [Asset] -> [Deviation]
+
+type Asset = Rational
+type Fraction = Rational
+type Contribution = Rational
+
+-- | Calculate the fractional part of each Asset in respect to the total Portfolio (list of Assets)
+fractions :: [Asset] -> Contribution -> [Fraction]
+fractions assets contribution = [ asset / totalValue |
+   let totalValue = toRational (sum assets) + contribution,
+   asset <- assets]
+
+type Target = Rational
+type Deviation = Rational
+
+
+-- [Target] must be summable to one!
+-- fractional deviations
+deviations :: [Target] -> [Fraction] -> [Deviation]
+deviations targets fractions = [ portion / target - 1 |
+                                 portion <- fractions |
+                                 target <- targets]
+
+-- bad name
+-- take the lowest deviation (asset), add the money to it until it matches to the next deviation, repeat until there's no money left
+calculate :: [Deviation] -> Contribution -> [Contribution]
+calculate deviations 0 = [0 | _ <- deviations]
+-- add other cases
